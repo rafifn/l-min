05d65605a3f25a9900c2a3b525186934306684f2
rafifn <rafiiferrynantyo@gmail.com>
2023-04-07 00:15:19 +0700

    bump version

--- src/module.ts
+++ src/module.ts
@@ -7,14 +7,15 @@ export default defineNuxtModule<ModuleOptions>({
   // Default configuration options of the Nuxt module
   defaults: {},
   setup (options, nuxt) {
-    // const currentDir = dirname(fileURLToPath(import.meta.url))
     const resolver = createResolver(import.meta.url)
 
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    // addPlugin(join(currentDir, './runtime/plugin'))
     addComponent({
       name: 'LCard',
       filePath: resolver.resolve('runtime/components/LCard.vue')
     })
+    if(nuxt.options.vite.optimizeDeps && nuxt.options.vite.optimizeDeps.include) {
+      nuxt.options.vite.optimizeDeps.include.push('element-plus', 'element-plus > dayjs', 'element-plus > dayjs/plugin/customParseFormat.js', 'element-plus > dayjs/plugin/localeData.js', 'dayjs/plugin/isSameOrAfter.js', 'dayjs/plugin/isSameOrBefore.js')
+    }
   }
 })
